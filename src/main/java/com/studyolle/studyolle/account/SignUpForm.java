ee22193708efbe21d8312bcf8f05d9be8695b199
ArouseSympathy <cleankid99@naver.com>
2020-08-24 22:12:23 +0900

    필드에 제한조건을 걸어줌 + 정규표현식

--- src/main/java/com/studyolle/studyolle/account/SignUpForm.java
+++ src/main/java/com/studyolle/studyolle/account/SignUpForm.java
@@ -2,12 +2,27 @@ package com.studyolle.studyolle.account;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class SignUpForm {
 
+    @NotBlank
+    @Length(min = 3, max = 20)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
     private String nickname;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @Length(min = 8, max = 50)
+    @NotBlank
     private String password;
 
 }
