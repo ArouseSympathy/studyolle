0ccec8e78926b1dd5c37fab0d26cded069e6a342
ArouseSympathy <cleankid99@naver.com>
2020-08-24 22:59:30 +0900

    메일 보내기 기능 작성

--- src/main/java/com/studyolle/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/studyolle/account/AccountController.java
@@ -1,6 +1,10 @@
 package com.studyolle.studyolle.account;
 
+import com.studyolle.studyolle.ConsoleMailSender;
+import com.studyolle.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,6 +20,8 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder){
@@ -33,6 +39,24 @@ public class AccountController {
         if(errors.hasErrors()){
             return "account/sign-up";
         }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // TODO 인코딩해야함
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+        Account newAccount = accountRepository.save(account);
+
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디올레, 회원 가입 인증"); //메일제목
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
+                "&email=" + newAccount.getEmail()); //본문
+        javaMailSender.send(mailMessage);
 
         //TODO 회원가입 처리
         return "redirect:/";
