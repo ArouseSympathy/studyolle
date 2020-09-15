43b1a63b6271f2c1105c3a6c1cb18c4acc8b21e7
ArouseSympathy <cleankid99@naver.com>
2020-09-16 00:12:33 +0900

    로그인 기억하기(Remember me 사용)

---
+++ src/main/java/com/studyolle/studyolle/domain/PersistentLogins.java
@@ -0,0 +1,27 @@
+package com.studyolle.studyolle.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@Table(name = "persistent_logins")
+@Entity
+public class PersistentLogins {
+
+    @Id
+    @Column(length = 64)
+    private String series;
+
+    @Column(nullable = false, length = 64)
+    private String username;
+
+    @Column(nullable = false, length = 64)
+    private String token;
+
+    @Column(name = "last_used", nullable = false, length = 64)
+    private LocalDateTime lastUsed;
+}
